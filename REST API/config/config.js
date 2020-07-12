8719bd7fb30ca821e77a945fbe80907afc9c8543
ggospodinov <baigrozdan99@abv.bg>
2020-07-12 04:51:19 +0300

    fix: remove not use things

---
+++ REST API/config/config.js
@@ -0,0 +1,12 @@
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+    development: {
+        port: process.env.PORT || 9999,
+        dbURL: 'mongodb+srv://user:softuni@softuni-ivota.mongodb.net/origami?retryWrites=true&w=majority',
+        authCookieName: 'x-auth-token'
+    },
+    production: {}
+};
+
+module.exports = config[env];
